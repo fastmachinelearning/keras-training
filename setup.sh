4ec4caf78a31175cd90f17ad880291b53a45d3f0
Javier Duarte <jduarte1@fnal.gov>
2017-11-21 17:47:22 +0000

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-1;95;0cexport PYTHONPATH=`pwd`/models:~src/tensorflow_anaconda2/tensorflow:$PYTHONPATH
+source activate keras-training
+#export PYTHONPATH=`pwd`/models:~src/tensorflow_anaconda2/tensorflow:$PYTHONPATH
 export KERASTRAINING=`pwd`
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
