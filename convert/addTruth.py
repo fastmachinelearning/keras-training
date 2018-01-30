1eb0ab1f6362e2392a0d0533606fd3d83bca8d08
Javier Duarte <jduarte1@fnal.gov>
2018-01-30 05:06:03 +0000

    update conv model; rotate input eta, phi

--- convert/addTruth.py
+++ convert/addTruth.py
@@ -6,6 +6,71 @@ from optparse import OptionParser
 import sys
 import numpy as np
 
+
+def delta_phi(phi1, phi2):
+  PI = 3.14159265359
+  x = phi1 - phi2
+  while x >=  PI:
+      x -= ( 2*PI )
+  while x <  -PI:
+      x += ( 2*PI )
+  return x
+
+# rotation + (possible) reflection needed later
+def rotate_and_reflect(x,y,w):
+    rot_x = []
+    rot_y = []
+    theta = 0
+    maxPt = -1
+    for ix, iy, iw in zip(x, y, w):
+        dR = np.sqrt(ix*ix+iy*iy)
+        thisPt = iw
+        if dR > 0.1 and thisPt > maxPt:
+            maxPt = thisPt
+            # rotation in eta-phi plane c.f  https://arxiv.org/abs/1407.5675 and https://arxiv.org/abs/1511.05190:
+            # theta = -np.arctan2(iy,ix)-np.radians(90)
+            # rotation by lorentz transformation c.f. https://arxiv.org/abs/1704.02124:
+            px = iw*np.cos(iy)
+            py = iw*np.sin(iy)
+            pz = iw*np.sinh(ix)
+            theta = np.arctan2(py,pz)+np.radians(90)
+            
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.matrix('{} {}; {} {}'.format(c, -s, s, -c))
+    for ix, iy, iw in zip(x, y, w):
+        # rotation in eta-phi plane:
+        #rot = R*np.matrix([[ix],[iy]])
+        #rix, riy = rot[0,0], rot[1,0]
+        # rotation by lorentz transformation
+        px = iw*np.cos(iy)
+        py = iw*np.sin(iy)
+        pz = iw*np.sinh(ix)
+        rot = R*np.matrix([[py],[pz]])
+        px1 = px
+        py1 = rot[0,0]
+        pz1 = rot[1,0]
+        iw1 = np.sqrt(px1*px1+py1*py1)
+        rix, riy = np.arcsinh(pz1/iw1), np.arcsin(py1/iw1)
+        rot_x.append(rix)
+        rot_y.append(riy)
+        
+    # now reflect if leftSum > rightSum
+    leftSum = 0
+    rightSum = 0
+    for ix, iy, iw in zip(x, y, w):
+        if ix > 0: 
+            rightSum += iw
+        elif ix < 0:
+            leftSum += iw
+    if leftSum > rightSum:
+        ref_x = [-1.*rix for rix in rot_x]
+        ref_y = rot_y
+    else:
+        ref_x = rot_x
+        ref_y = rot_y
+    
+    return np.array(ref_x), np.array(ref_y)
+
 def getTree(myTree, oldTree, listOfBranches, additionalBranches = []):
     
     rando = random.randint(1,999999)
@@ -45,7 +110,7 @@ def addLeaves(tree,fileName):
     particleBranches = [branch.GetName() for branch in tree.GetListOfBranches() if 'j1_' in branch.GetName()]
     additionalBranches = []
     if len(particleBranches)>0:
-        additionalBranches = ['j1_erel','j1_pt','j1_ptrel','j1_eta','j1_etarel','j1_phi','j1_phirel','j1_deltaR']
+        additionalBranches = ['j1_erel','j1_pt','j1_ptrel','j1_eta','j1_etarel','j1_etarot','j1_phi','j1_phirel','j1_phirot','j1_deltaR']
 
     tree.SetBranchStatus("*",1)
     # remove these branches
@@ -104,6 +169,12 @@ def addLeaves(tree,fileName):
                 particleEta.append( particle.Eta() )
                 particlePhi.append( particle.Phi() )
                 particleE.append( particle.E() )
+            maxPtIndex = np.argmax([pt for pt in particlePt])
+            x = np.array(particleEta) - particleEta[maxPtIndex]*np.ones(len(particleEta))
+            delta_phi_func = np.vectorize(delta_phi)
+            y = delta_phi_func(np.array(particlePhi), particlePhi[maxPtIndex])
+            w = np.array(particlePt)
+            x, y = rotate_and_reflect(x, y, w)
             for i_particle in np.argsort([-pt for pt in particlePt]):
                 particle = rt.TLorentzVector(tree.j1_px[i_particle], tree.j1_py[i_particle], tree.j1_pz[i_particle], tree.j1_e[i_particle])
                 #if particlePt[i_particle] < 1: continue
@@ -114,8 +185,10 @@ def addLeaves(tree,fileName):
                 setattr(s1, 'j1_ptrel', float(particlePt[i_particle]/tree.j_pt[0]))
                 setattr(s1, 'j1_eta', float(particleEta[i_particle]))
                 setattr(s1, 'j1_etarel', float(particleEta[i_particle]-tree.j_eta[0]))
+                setattr(s1, 'j1_etarot', float(x[i_particle]))
                 setattr(s1, 'j1_phi', float(particlePhi[i_particle]))
-                setattr(s1, 'j1_phirel', float(particlePhi[i_particle]-jet.Phi()))
+                setattr(s1, 'j1_phirel', float(delta_phi(particlePhi[i_particle],jet.Phi())))
+                setattr(s1, 'j1_phirot', float(y[i_particle]))
                 setattr(s1, 'j1_deltaR', float(jet.DeltaR(particle)))
                 newtree.Fill()
         else:
